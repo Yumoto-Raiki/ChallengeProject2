b7031b4378944f90f6a7998d80146b2eb6f55b1b
CSPC2024-08\CS <yumoto0220.business@gmail.com>
2025-05-12 19:41:52 +0900

    no message

--- Assets/Scriptable/EnemyStatus.cs
+++ Assets/Scriptable/EnemyStatus.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBase : MonoBehaviour
+[CreateAssetMenu(menuName = "ScriptableObject/EnemyStatus")]
+public class EnemyStatus : ScriptableObject
 {
+
+
     // Start is called before the first frame update
     void Start()
     {
